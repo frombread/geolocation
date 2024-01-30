3b3a4dd6b6374fa13155874eaf448573b810f9ae
frombread <gon03004@gmail.com>
2024-01-30 19:31:34 +0900

    view 추가

--- src/main/java/com/geolocation/country/service/FindIpController.java
+++ src/main/java/com/geolocation/country/service/FindIpController.java
@@ -1,23 +1,25 @@
 package com.geolocation.country.service;
 
 import java.net.UnknownHostException;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FindIpController {
-    private final RedisService redisService;
 
-    public FindIpController(RedisService redisService){
-      this.redisService = redisService;
-    }
+  private final RedisService redisService;
 
+  public FindIpController(RedisService redisService) {
+    this.redisService = redisService;
+  }
 
-    @GetMapping("/find/{key}")
-    public String findIp(@PathVariable String key) {
-      System.out.println(key);
-      long LongKey = redisService.ipToLong(key);
-      return redisService.getCountriesInRangeByScore(LongKey);
-    }
+  @GetMapping("/find/{key}")
+  public String findIp(@PathVariable String key) {
+    System.out.println(key);
+    long LongKey = redisService.ipToLong(key);
+    return redisService.getCountriesInRangeByScore(LongKey);
+  }
 }
